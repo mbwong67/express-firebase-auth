f787ff4c10d6626d04699a3a02a953faf22a4228
sergentemel <sergent@vision.tech>
2023-08-22 02:35:32 +0300

    fix: path of logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -8,8 +8,8 @@ const logger = winston.createLogger({
         winston.format.colorize({ all: true }),
     ),
     transports: [
-        new winston.transports.File({ filename: '../logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: '../logs/combined.log' }),
+        new winston.transports.File({ filename: 'src/logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'src/logs/combined.log' }),
     ],
 });
 
